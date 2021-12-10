fbf39c2da821dc7c02e20136ed56aa2958cc5005
hungnguyen_2k <hungnm@monsterstudio.io>
2021-12-10 17:35:38 +0700

    done

--- assets/Script/HandleBlock.js
+++ assets/Script/HandleBlock.js
@@ -4,12 +4,15 @@ cc.Class({
 
     properties: {
     },
+    onBeginContact(contact, selfCollider, otherCollider) {
+        // selfCollider.enabled = false;
+    },
     start() {
         this.node.children[window.colorChoose].on('touchmove', function (event) {
             event.target.opacity = 10
         });
     },
 
-    update (dt) {
+    update(dt) {
     },
 });
--- assets/Script/HandleTouch.js
+++ assets/Script/HandleTouch.js
@@ -1,9 +1,19 @@
+
+window.currentPixelFilled = 0;
 cc.Class({
     extends: cc.Component,
     onBeginContact: function (contact, selfCollider, otherCollider) {
+        //cut name to take number and color to fill color
         if (window.colorChoose == otherCollider.node.name.slice(0, 1)) {
-            otherCollider.node.children[0].color = new cc.Color().fromHEX(`${otherCollider.node.name.slice(2, 9)}`)
-            otherCollider.node.children.slice(1).map((item) => item.opacity = 0)
+            otherCollider.node.name = `${otherCollider.node.name}-filled`;
+            otherCollider.node.children[0].color = new cc.Color().fromHEX(`${otherCollider.node.name.slice(2, 9)}`),
+                otherCollider.enabled = false;
+            window.totalPixel--;
+            window.currentPixelFilled++;
+            //hide all node number orther 
+            otherCollider.node.children.slice(1).map((item) => {
+                item.opacity = 0;
+            });
         }
     }
 });
